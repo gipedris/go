562b16946b310177734bddd41e85f2fec1d8c92f
John Doe <johndoe@example.com>
2024-10-01 00:48:18 +0300

    hw 2

--- Homework/task2/m1_t2_hw.go
+++ Homework/task2/m1_t2_hw.go
@@ -16,13 +16,8 @@ func main() {
 	var input int
 	fmt.Println("Введите трехзначное число: ")
 	fmt.Scan(&input)
-	// сколько целых сотен
 	first := input / 100
-	fmt.Println("Первая цифра", first)
-	// сколько целых десятков
 	second := input % 100 / 10
-	fmt.Println("Вторая цифра", second)
-	// сколько единиц
 	third := input % 10
-	fmt.Println("Третья цифра", third)
+	fmt.Printf("%v%v%v\n", third, second, first)
 }
