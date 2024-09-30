6c916189abfaeacfd43d25f2fee2f90811749704
John Doe <johndoe@example.com>
2024-10-01 01:47:20 +0300

    task 3

--- Homework/task3/m2_t1_hw.go
+++ Homework/task3/m2_t1_hw.go
@@ -9,4 +9,48 @@
 */
 package main
 
-// Ваш код
+import (
+	"fmt"
+)
+
+func main() {
+
+	var (
+		a, b, c, x int
+	)
+
+	fmt.Println("Введите первое число: ")
+	fmt.Scan(&a)
+	fmt.Println("Введите второе число: ")
+	fmt.Scan(&b)
+	fmt.Println("Введите третье число: ")
+	fmt.Scan(&c)
+	fmt.Printf("Ввод: %v %v %v\n", a, b, c)
+
+	if b > c {
+		x = b
+		b = c
+		c = x
+	}
+
+	if a > c {
+		x = a
+		a = c
+		c = x
+	}
+
+	if a > b {
+		x = a
+		a = b
+		b = x
+	}
+
+	fmt.Printf("Вывод: %v %v %v\n", a, b, c)
+}
+
+/*
+	9 6 3
+	9 3 6
+	6 3 9
+	3 6 9
+*/
